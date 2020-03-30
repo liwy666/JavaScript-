68123b27af70bad967d7c09f94d58ea0dbce15f6
liwy <987303897@qq.com>
2020-03-30 23:14:19 +0800

    '严格模式完成'

---
+++ es6学习/函数.js
@@ -0,0 +1,18 @@
+/*命名函数*/
+function fun() {
+
+}
+/*2.函数表达式（匿名函数）*/
+var fun = function () {
+
+};
+/*3.利用new Function*/
+var f = new Function('a','b','console.log(a+b)');
+
+/**this指向*/
+/*普通函数，window*/
+/*构造函数，实例*/
+/*对象方法，该方法所属对象*/
+/*事件绑定，绑定事件对象*/
+/*定时器，window*/
+/*计时器，window*/
